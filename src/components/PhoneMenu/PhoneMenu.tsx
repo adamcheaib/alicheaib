2464160d63d67239c17ef7a0ad51848debdf2c89
Adam <adamhcheaib@gmail.com>
2025-06-17 16:20:41 +0200

    hamburger menu updated

--- src/components/PhoneMenu/PhoneMenu.tsx
+++ src/components/PhoneMenu/PhoneMenu.tsx
@@ -1,6 +1,8 @@
 import Offcanvas from "react-bootstrap/Offcanvas"
 import {ContextPhoneMenu, type ShowPhoneMenuProps} from "../../contexts/ContextPhoneMenu.tsx";
 import {useContext} from "react";
+import {Stack, Dropdown} from "react-bootstrap";
+import "./PhoneMenu.css"
 
 
 function PhoneMenu(): React.ReactElement {
@@ -17,9 +19,23 @@ function PhoneMenu(): React.ReactElement {
                     <Offcanvas.Title>Ali Cheaib</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur debitis, delectus dolore et
-                    excepturi exercitationem illo labore magni minima molestiae numquam officiis pariatur quam quas
-                    quidem repellendus saepe similique ut?
+                    <Stack className="phoneMenu" style={{alignItems: "center"}}>
+                        <a className="phoneOption" href="#">Home</a>
+                        <a className="phoneOption" href="#">About me</a>
+                        <a className="phoneOption" href="#">Bireme Studios</a>
+                        <Dropdown drop="down-centered" className="dropDownBtn">
+                            <Dropdown.Toggle>
+                                Categories
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item className="phoneOption" href="#">Artwork</Dropdown.Item>
+                                <Dropdown.Item className="phoneOption" href="#">3D</Dropdown.Item>
+                                <Dropdown.Item className="phoneOption" href="#">Graphic Design</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+
+                    </Stack>
                 </Offcanvas.Body>
             </Offcanvas>
         </>
